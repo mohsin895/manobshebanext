d427435ee8b59b6de517e6ca20d8b0f863903a3e
Essential-Infotech <33011566+mohsin895@users.noreply.github.com>
2026-08-01 13:26:36 +0600

    updat syllsbus

--- components/componentSecond.tsx
+++ components/componentSecond.tsx
@@ -37,10 +37,10 @@ export function ComponentSecond() {
           </div>
 
           <div className='flex justify-end'>
-            <button className='inline-flex items-center text-[12px] gap-2 rounded-full bg-[#FE4711] px-4 py-2 text-white'>
+            <a href='/syllabus' className='inline-flex items-center text-[12px] gap-2 rounded-full bg-[#FE4711] px-4 py-2 text-white'>
               {t('scholarship.see_more')}
               <ArrowUpRight className='h-4 w-4' />
-            </button>
+            </a>
           </div>
         </div>
       </div>
