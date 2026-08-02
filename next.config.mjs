883d989ba9e5b6158b0ccf31b0e0579ae9f8264d
Essential-Infotech <33011566+mohsin895@users.noreply.github.com>
2026-08-03 02:40:03 +0600

    update image optimize

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,31 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   images: {
+//     unoptimized: true,
+//   },
+// }
+//
+// export default nextConfig
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
-    unoptimized: true,
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    minimumCacheTTL: 60,
   },
 }
 
